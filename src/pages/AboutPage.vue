20db96dfd5dda0ffda3ad49a7dbd6ade3e60739d
Madison Yarnall <maddycodes01@gmail.com>
2022-01-21 13:40:59 -0700

    change

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="about">
-    <h1>This is the about page</h1>
+  <div class="profile">
+    <h1>This is the profile page</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'AboutPage'
+  name: 'ProfilePage'
 }
 </script>
